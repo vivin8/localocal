a30c0b5c0bb4eed3387a4a44d0aa7cb42d5b2300
Viktor Vinnik <viktor.vinnik@ikea.com>
2018-10-05 19:11:08 -0400

    big update

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-'use strict';
+var http = require('http')
+var auth = require('basic-auth')
+var compare = require('tsscmp')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
+var baseDirectory = __dirname
 
-var fs = require('fs');
-var path = require('path');
+// Create server
+var server = http.createServer(function (req, res) {
+  var credentials = auth(req)
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+  // Check credentials
+  // The "check" function will typically be against your user store
+  if (!credentials || !check(credentials.name, credentials.pass)) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    // res.end('Access granted')
+    var requestUrl = url.parse(req.url)
 
-  callback(null, result);
-};
+    // need to use path.normalize so people can't access directories underneath baseDirectory
+    var fsPath = baseDirectory+path.normalize('/public/'+requestUrl.pathname)
+
+    var fileStream = fs.createReadStream(fsPath)
+    console.log(fsPath);
+    fileStream.pipe(res)
+    fileStream.on('open', function() {
+        res.writeHead(200)
+    })
+    fileStream.on('error',function(e) {
+        res.writeHead(404)     // assume the file doesn't exist
+        res.end()
+    })
+  }
+})
+
+// Basic function to validate credentials for example
+function check (name, pass) {
+  var valid = true
+
+  // Simple method to prevent short-circut and use timing-safe compare
+  valid = compare(name, 'vivivn8') && valid
+  valid = compare(pass, 'vivin8') && valid
+
+  return valid
+}
+
+// Listen
+server.listen(3000)
